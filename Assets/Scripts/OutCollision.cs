1505e773a41715cd2cf1dd8e278a6b2fd4942475
Artem Frolov <frolovart161@gmail.com>
2021-02-18 18:22:44 +0700

    Adding collisions.

---
+++ Assets/Scripts/OutCollision.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+    }
+}
